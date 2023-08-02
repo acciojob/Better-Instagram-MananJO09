e64c9446421d19e09ef2d1702bf6ba58ffae832e
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-08-02 16:23:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+const imgBox= document.querySelector('.image');
+const divBox = document.getElementsByClassName('.flex');
+
+imgBox.addEventListener('dragstart', (e) => {
+	e.target.className +=' selected0';
+})
