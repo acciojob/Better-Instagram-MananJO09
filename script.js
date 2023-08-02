02049493d2b18d667de2fa7783f4108435ea36b1
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-08-02 16:48:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-//your code here
-const imgBox= document.querySelector('.image');
-const divBoxes = document.getElementsByClassName('.flex');
-
-imgBox.addEventListener('dragstart', (e) => {
-	e.target.className +=' selected';
-
-	setTimeout(() => {
-		e.target.className = 'hide';	
-	} ,0);
-	
-});
-
-imgBox.addEventListener('dragend', (e) => {
-	e.target.className ='imgBox';
-})
-
-for (divBoxes  of divBox) {
-	divBox.addEventListener('dragover', (e) => {
-		e.preventDefault();
-		
-	});
-	divBox.addEventListener('dragenter', () => {
-		
-	});
-	divBox.addEventListener('dragleave', () => {
-		
-	});
-	divBox.addEventListener('drop', (e) => {
-		e.target.append(imgBox);
-	});
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+ 
+const images = document.querySelectorAll(".image");
+ 
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
 }
+ 
+function allowDrop(e) {
+  e.preventDefault();
+}
+ 
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
+  }
+ 
+  dragdrop(clone);
+ 
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+ 
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
+}
+ 
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+ 
+images.forEach(dragdrop);
