eafe5a8ba40c8ab23325b73b0189a3087bab43c8
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-08-02 16:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const divBoxes = document.getElementsByClassName('.flex');
 
 imgBox.addEventListener('dragstart', (e) => {
 	e.target.className +=' selected';
+
+	setTimeout(() => {
+		e.target.className = 'hide';	
+	} ,0);
+	
 })
 
 for (divBoxes  of divBox) {
-	divBox.addEventListener('dragover', () => {
-		
+	divBox.addEventListener('dragover', (e) => {
+		e.preventDefault();
+		e.target.className ='imgBox';
 	});
 	divBox.addEventListener('dragenter', () => {
 		
@@ -16,7 +22,7 @@ for (divBoxes  of divBox) {
 	divBox.addEventListener('dragleave', () => {
 		
 	});
-	divBox.addEventListener('drop', () => {
-		
+	divBox.addEventListener('drop', (e) => {
+		e.target.append(imgBox);
 	});
 }
